041b2033454bd877caa69445245bfab921fc0ab7
Song Yang <songyang1218@gmail.com>
2020-07-31 22:47:05 -0400

    Resolve silence problem

---
+++ Assets/Scripts/ButtonAudioTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudioTest : MonoBehaviour
+{
+    public void WhenClicked()
+    {
+        GetComponent<FMODUnity.StudioEventEmitter>().Play();
+        Debug.Log("clicked");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Delay(5f));
+    }
+
+    IEnumerator Delay(float t)
+    {
+        yield return new WaitForSeconds(t);
+        GetComponent<FMODUnity.StudioEventEmitter>().Play();
+    }
+}
