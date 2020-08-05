aa324409cb77bec72c561b601c9f4e43005fec48
Song Yang <songyang1218@gmail.com>
2020-08-05 14:52:25 -0400

    Change monster killing process and introduce VHS player
    Co-authored-by: Penca53 <pencapencator@gmail.com>

---
+++ Assets/Scripts/TapeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages the tape mechanics and interacts with the linked monster
+/// </summary>
+public class TapeManager : MonoBehaviour
+{
+    // Reference to the Monster linked to this tape
+    public MonsterAI monsterAI;
+
+    // Called whenever we interact with the tape
+    /*
+    public void Interact()
+    {
+        monsterAI?.OnTapeInteract();
+    }
+    */
+}
