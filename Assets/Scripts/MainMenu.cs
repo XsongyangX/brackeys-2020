fa98a2aecded8a61933ec1d0f33d432900c92168
Song Yang <songyang1218@gmail.com>
2020-08-04 23:31:20 -0400

    Add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Decides what happens when buttons are clicked on the main menu
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+
+    /// <summary>
+    /// String of the name of the scene containing the level.
+    /// that scene must be in the build settings.
+    /// </summary>
+    [Tooltip("Name of the scene of the level")]
+    [SerializeField]
+    private string levelName = "LevelTest";
+
+    /// <summary>
+    /// Empty containing the main menu buttons
+    /// </summary>
+    public GameObject MainButtons;
+
+    /// <summary>
+    /// Empty containing the credits
+    /// </summary>
+    public GameObject Credits;
+
+    public void GoToLevel()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void ShowCredits()
+    {
+        Credits.SetActive(true);
+        MainButtons.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMain()
+    {
+        Credits.SetActive(false);
+        MainButtons.SetActive(true);
+    }
+}
