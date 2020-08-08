528c0ba44e54bfe0e25b949baaf7586c73424fdc
Song Yang <songyang1218@gmail.com>
2020-08-07 20:06:57 -0400

    Add rewind death event

---
+++ Assets/Scripts/VHSPlayerAudio.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class VHSPlayerAudio : MonoBehaviour
+{
+    public StudioEventEmitter RewindDeath;
+}
